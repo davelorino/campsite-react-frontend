e1ea80b28c1f511f6b65ce8657e1f88f021fed0d
davelorino <davelorino@gmail.com>
2019-09-08 02:10:58 +1000

    conditionally render menu items

--- Ecommerce/ecommerce-front/ecommerce-front/src/auth/index.js
+++ Ecommerce/ecommerce-front/ecommerce-front/src/auth/index.js
@@ -42,3 +42,31 @@ export const signup = user => {
       next();
     }
   };
+  
+  export const signout = (next) => {
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("jwt");
+      next();
+      return fetch(`${API}/signout`, {
+        method: "GET"
+      })
+      .then(response => {
+        console.log('signout', response);
+      })
+      .catch(err => console.log(err));
+    }
+  };
+  
+  export const isAuthenticated = () => {
+    if(typeof window == 'undefined') {
+      return false;
+    }
+    if(localStorage.getItem('jwt')) {
+      return JSON.parse(localStorage.getItem('jwt'));
+    } else {
+      return false;
+    }
+  };
+  
+  
+  
