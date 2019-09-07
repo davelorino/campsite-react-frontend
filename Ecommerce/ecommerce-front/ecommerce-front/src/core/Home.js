1cc3b0be483fe3ebf4082bc914668d37fdf6e836
davelorino <davelorino@gmail.com>
2019-09-07 16:48:53 +1000

    UI progress

---
+++ Ecommerce/ecommerce-front/ecommerce-front/src/core/Home.js
@@ -0,0 +1,15 @@
+// core/Home.js
+
+import React from 'react';
+import Layout from './Layout';
+
+const Home = () => (
+  <Layout title="Home Page" description="Node React E-commerce App">
+  
+  </Layout>
+);
+
+export default Home;
+
+
+
---
+++ Ecommerce/ecommerce-front/ecommerce-front/src/user/Signin.js
@@ -0,0 +1,17 @@
+// users Signin
+
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Signin = () => (
+  <Layout title="Signin" description="Welcome back.">
+      ...
+  </Layout>
+);
+
+export default Signin;
+
+
+
+
+
