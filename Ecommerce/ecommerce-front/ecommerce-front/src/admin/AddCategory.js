0817378731de6786287f498d04e1cfbe3fb8cba3
davelorino <davelorino@gmail.com>
2019-09-08 22:07:31 +1000

    gradient background and create category for admin users

---
+++ Ecommerce/ecommerce-front/ecommerce-front/src/admin/AddCategory.js
@@ -0,0 +1,83 @@
+// admin AddCategory.js
+
+import React, {useState} from "react";
+import Layout from '../core/Layout';
+import {isAuthenticated} from '../auth';
+import {Link} from 'react-router-dom';
+import {createCategory} from './apiAdmin';
+
+const AddCategory = () => {
+  const [name, setName] = useState('');
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  
+  const {user, token} = isAuthenticated();
+  
+  const handleChange = (e) => {
+      setError('');
+      setName(e.target.value);
+  };
+  
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    setError('');
+    setSuccess(false);
+    createCategory(user._id, token, {name})
+    .then(data => {
+      if(data.error) {
+        setError(data.error);
+      } else {
+        setError("");
+        setSuccess(true);
+        }
+      });
+  };
+  
+  const newCategoryForm = () => (
+    <form onSubmit={clickSubmit}>
+        <div className="form-group">
+            <label className="text-muted">Category Name</label>
+                <input type="text" 
+                 className="form-control" 
+                 onChange={handleChange} 
+                 value={name}
+                 autoFocus
+                 />
+                 <button className="btn btn-outline-dark mt-2">Create Category</button>
+        </div>
+    </form>
+  );
+  
+  const showSuccess = () => {
+    if(success) {
+      return <h5 className="text-info">{name} is created</h5>
+    }
+  };
+  
+  const showError = () => {
+    if(error) {
+      return <h5 className="text-danger">{name} should be unique.</h5>
+    }
+  };
+  
+  return(
+      <Layout title="Category" description="Manage your projects more effectively with categories." className="container">
+          <div className="row">
+              <div className="col-md-8 offset-md-2">
+                  {showSuccess()}
+                  {showError()}
+                  {newCategoryForm()}
+              </div>
+          </div>
+      </Layout>
+  );
+  
+};
+
+
+export default AddCategory;
+
+
+
+
+
--- Ecommerce/ecommerce-front/ecommerce-front/src/core/Layout.js
+++ Ecommerce/ecommerce-front/ecommerce-front/src/core/Layout.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Menu from './Menu';
+import "../styles.css";
 
 const Layout = ({ title = 'Title', 
                   description = 'Description', 
