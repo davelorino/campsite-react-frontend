9f710bb4227a0f45299c8074d16fb40d797c71a4
davelorino <davelorino@gmail.com>
2019-09-16 14:53:50 +1000

    added apply for project backend functionality

--- Ecommerce/ecommerce-front/ecommerce-front/src/core/Card.js
+++ Ecommerce/ecommerce-front/ecommerce-front/src/core/Card.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, Redirect} from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
+import {addItem} from './helpers';
 
 const Card = ({project, showViewProjectButton = true}) => {
+  const [redirect, setRedirect] = useState(false);
   const showViewButton = (showViewProjectButton) => {
     return(
       showViewProjectButton && (
@@ -16,9 +18,21 @@ const Card = ({project, showViewProjectButton = true}) => {
     );
   };
   
+  const addToCart = () => {
+    addItem(project, () => {
+      setRedirect(true);
+    });
+  };
+  
+  const shouldRedirect = redirect => {
+    if(redirect) {
+      return <Redirect to="/cart" />
+    }
+  };
+  
   const showApplyButton = () => {
     return (
-      <button className="btn btn-outline-info mt-2 mb-2">
+      <button onClick={addToCart} className="btn btn-outline-info mt-2 mb-2">
           Apply 
       </button>
     );
@@ -28,6 +42,7 @@ const Card = ({project, showViewProjectButton = true}) => {
         <div className="card">
             <div className="card-header name">{project.name}</div>
             <div className="card-body">
+            {shouldRedirect(redirect)}
                 <ShowImage item={project} url="project" />
                 <p className="Lead mt-2">{project.description.substring(0, 100)}</p>
                 <p className="black-10">${project.pitch_price}</p>
